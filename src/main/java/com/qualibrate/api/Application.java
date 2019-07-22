d5d91fd67ce2f14adad2fb7edeff8fe16cc8a13a
Chetan <chetan.shirke@pb.com>
2019-07-23 00:26:33 +0530

    task-0014: fix beanstalk cloud autoconfiguration issues.

--- src/main/java/com/qualibrate/api/Application.java
+++ src/main/java/com/qualibrate/api/Application.java
@@ -1,7 +1,11 @@
 package com.qualibrate.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration;
 
 /**
  *
@@ -9,6 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        ElastiCacheAutoConfiguration.class,
+        MailSenderAutoConfiguration.class,
+        ContextStackAutoConfiguration.class
+    })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
