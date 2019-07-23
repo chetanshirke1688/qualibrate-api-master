bf6ca85cabcaabd52c117dc84dd33e4004b043d5
Chetan <chetan.shirke@pb.com>
2019-07-24 00:34:01 +0530

    BDA-0017: Add appdynamics java agent.

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
 
-exec java -jar /opt/app/server.jar --spring.profiles.active=test --spring.cloud.config.enabled=false
+JAVA_OPTS="-javaagent:/opt/appdynamics/app-agent/javaagent.jar"
+
+exec java $JAVA_OPTS -jar /opt/app/server.jar --spring.profiles.active=test --spring.cloud.config.enabled=false
