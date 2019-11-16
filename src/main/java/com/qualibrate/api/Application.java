61cd135af37508d02ea71a3f7b9e04013c71267e
Chetan <chetan.shirke@pb.com>
2019-11-16 16:30:40 +0530

    BDA-9999: Added Sysout

--- src/main/java/com/qualibrate/api/Application.java
+++ src/main/java/com/qualibrate/api/Application.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.qualibrate.api.*")
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Launching Application...");
         SpringApplication.run(Application.class, args);
     }
 }
